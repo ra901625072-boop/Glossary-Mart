18e3a4356da1a49e16266c8c8ebd83e280cfef33
Akshay Rajput <ra901625072@gmail.com>
2026-05-09 11:35:02 +0530

    update

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
 
-# Run any necessary build steps
-# (Optional) python manage.py migrate
+# Run database initialization/migrations
+# We can't easily run the python script here if it needs a full app context and DB access
+# but we can try. If it fails, the deploy will fail early which is better.
+# python -c "from app import create_app, init_db; app=create_app(); init_db(app)"
